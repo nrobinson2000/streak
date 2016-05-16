5d40fabc322afe19b1ae0ac42050401f044f571c
Nathan Robinson <nrobinson2000@me.com>
2016-05-16 12:10:03 +0100

    12:10 of 16-05-2016

--- streak.sh
+++ streak.sh
@@ -3,6 +3,6 @@ commit_date=" $(date +"%H:%M") of $(date +"%d-%m-%Y")"
 rm date
 echo $commit_date > date
 git add -A
-git commit -m "$1 at $(date +"%H:%M") of $(date +"%d-%m-%Y")"
+git commit -m "$(date +"%H:%M") of $(date +"%d-%m-%Y")"
 git push origin $(git rev-parse --abbrev-ref HEAD)
 
